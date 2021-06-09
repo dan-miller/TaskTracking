a9e83df35505a78534073374cc5be132a989f802
Dan Miller <Dan.Miller@blackbaud.me>
2021-06-09 12:57:38 -0400

    Add project files.

---
+++ TaskTracking.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace TaskTracking
+{
+    partial class TaskTracking
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_Start = new System.Windows.Forms.Button();
+            this.b_Stop = new System.Windows.Forms.Button();
+            this.l_Time = new System.Windows.Forms.Label();
+            this.c_TaskSelection = new System.Windows.Forms.ComboBox();
+            this.l_WriteSuccess = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b_Start
+            // 
+            this.b_Start.Location = new System.Drawing.Point(33, 143);
+            this.b_Start.Name = "b_Start";
+            this.b_Start.Size = new System.Drawing.Size(75, 23);
+            this.b_Start.TabIndex = 0;
+            this.b_Start.Text = "Start";
+            this.b_Start.UseVisualStyleBackColor = true;
+            this.b_Start.Click += new System.EventHandler(this.b_Start_Click);
+            // 
+            // b_Stop
+            // 
+            this.b_Stop.Location = new System.Drawing.Point(119, 143);
+            this.b_Stop.Name = "b_Stop";
+            this.b_Stop.Size = new System.Drawing.Size(75, 23);
+            this.b_Stop.TabIndex = 1;
+            this.b_Stop.Text = "Stop";
+            this.b_Stop.UseVisualStyleBackColor = true;
+            this.b_Stop.Click += new System.EventHandler(this.b_Stop_Click);
+            // 
+            // l_Time
+            // 
+            this.l_Time.AutoSize = true;
+            this.l_Time.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.l_Time.Location = new System.Drawing.Point(33, 26);
+            this.l_Time.Name = "l_Time";
+            this.l_Time.Size = new System.Drawing.Size(161, 32);
+            this.l_Time.TabIndex = 2;
+            this.l_Time.Text = "00:00:00.000";
+            // 
+            // c_TaskSelection
+            // 
+            this.c_TaskSelection.FormattingEnabled = true;
+            this.c_TaskSelection.Location = new System.Drawing.Point(33, 98);
+            this.c_TaskSelection.Name = "c_TaskSelection";
+            this.c_TaskSelection.Size = new System.Drawing.Size(161, 23);
+            this.c_TaskSelection.TabIndex = 3;
+            // 
+            // l_WriteSuccess
+            // 
+            this.l_WriteSuccess.AutoSize = true;
+            this.l_WriteSuccess.Location = new System.Drawing.Point(33, 80);
+            this.l_WriteSuccess.Name = "l_WriteSuccess";
+            this.l_WriteSuccess.Size = new System.Drawing.Size(0, 15);
+            this.l_WriteSuccess.TabIndex = 4;
+            // 
+            // TaskTracking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 181);
+            this.Controls.Add(this.l_WriteSuccess);
+            this.Controls.Add(this.c_TaskSelection);
+            this.Controls.Add(this.l_Time);
+            this.Controls.Add(this.b_Stop);
+            this.Controls.Add(this.b_Start);
+            this.KeyPreview = true;
+            this.Name = "TaskTracking";
+            this.Text = "Task Tracking";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b_Start;
+        private System.Windows.Forms.Button b_Stop;
+        private System.Windows.Forms.Label l_Time;
+        private System.Windows.Forms.ComboBox c_TaskSelection;
+        private System.Windows.Forms.Label l_WriteSuccess;
+    }
+}
+
---
+++ TaskTracking.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskTracking
+{
+    public partial class TaskTracking : Form
+    {
+        private System.Timers.Timer timer;
+        private Stopwatch stopwatch;
+        private List<string> tasks;
+        public TaskTracking()
+        {
+            InitializeComponent();
+
+            this.TopLevel = true;
+
+            stopwatch = new Stopwatch();
+            timer = new System.Timers.Timer();
+            timer.Interval = 1;
+            timer.Elapsed += Timer_Elapsed;
+
+            var fdr = new StreamReader("tasks.ini");
+            tasks = new List<string>();
+
+            while (!fdr.EndOfStream)
+            {
+                tasks.Add(fdr.ReadLine().Trim());
+            }
+
+            fdr.Close();
+            tasks.Sort();
+            c_TaskSelection.Items.AddRange(tasks.ToArray());
+        }
+
+        delegate void SetTimerTextCallback(string text);
+
+        private void SetTimerText(string text)
+        {
+            if (this.l_Time.InvokeRequired)
+            {
+                var _dispatcher_ = new SetTimerTextCallback(SetTimerText);
+                this.Invoke(_dispatcher_, new object[] { text });
+            }
+            else
+            {
+                this.l_Time.Text = text;
+            }
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            var timerText = stopwatch.Elapsed.ToString(@"hh\:mm\:ss\.fff");
+            SetTimerText(timerText);
+        }
+
+        private void b_Start_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            stopwatch.Reset();
+            stopwatch.Start();
+        }
+
+        private void b_Stop_Click(object sender, EventArgs e)
+        {
+            stopwatch.Stop();
+            timer.Stop();
+
+            var messageTimer = new System.Timers.Timer() { Interval = 15000, AutoReset = false };
+            messageTimer.Elapsed += MessageTimer_Elapsed;
+
+            if (stopwatch.ElapsedMilliseconds == 0) return;
+
+            if (c_TaskSelection.SelectedItem == null || c_TaskSelection.SelectedItem.ToString() == string.Empty)
+            {
+                l_WriteSuccess.Text = "No data written";
+                messageTimer.Start();
+            }
+            else
+            {
+                var elapsedHours = (int)Math.Ceiling((double)stopwatch.ElapsedMilliseconds / 1000 / 60 / 60);
+                var fdw = new StreamWriter("tasks.csv", true);
+                fdw.WriteLine($"{DateTime.Today.ToShortDateString()},{c_TaskSelection.SelectedItem.ToString()},{elapsedHours.ToString()}");
+
+                l_WriteSuccess.Text = $"{elapsedHours} hours -> {c_TaskSelection.SelectedItem}";
+                messageTimer.Start();
+                fdw.Close();
+
+            }
+
+        }
+
+        delegate void ClearMessageTimerCallback();
+
+        private void ClearMessageTimer()
+        {
+            if (this.l_WriteSuccess.InvokeRequired)
+            {
+                var _dispatcher_ = new ClearMessageTimerCallback(ClearMessageTimer);
+                this.Invoke(_dispatcher_);
+            }
+            else
+            {
+                this.l_WriteSuccess.Text = string.Empty;
+            }
+
+        }
+
+        private void MessageTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            ClearMessageTimer();
+        }
+    }
+}
